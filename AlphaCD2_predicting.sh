89a039d87a7dc0dd5e4627d18c505398ebcd3be5
Mingdi Wu <mingmingdi754@gmail.com>
2026-08-04 09:36:18 +0800

    Delete AlphaCD2_predicting.sh

--- AlphaCD2_predicting.sh
+++
@@ -1,1 +0,0 @@
-python AlphaCD2_predicting.py --input test.txt --model $Path/ensemble_model.pth --scaler $Path/scaler.pkl --esm_weights $Path/esmc_600m_2024_12_v0.pth --output test_efficiency_pre.txt --use_gpu false
